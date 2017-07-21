4551256bdbebf2b7e0b07c1de651da2013639e67
Greg Landrum <greg.landrum@gmail.com>
2017-07-21 06:18:34 +0200

    first pass

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ChemTopicModel',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://rdkit.org',
+      author='Nadine Schneider',
+      author_email='nadine-1.schneider@novartis.com',
+      license='BSD',
+      packages=['ChemTopicModel'],
+      zip_safe=False)
