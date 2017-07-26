9f8b8f5ab697b447115672e2c08127c9b068922f
Greg Landrum <greg.landrum@gmail.com>
2017-07-26 05:34:38 +0200

    an actual description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='ChemTopicModel',
       version='0.1',
-      description='The funniest joke in the world',
+      description='Applying topic modeling for chemistry. https://dx.doi.org/10.1021/acs.jcim.7b00249',
       url='http://rdkit.org',
       author='Nadine Schneider',
       author_email='nadine.schneider.shb@gmail.com',
